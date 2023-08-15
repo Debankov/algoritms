f1e37cb48aa917d881d9b3b1570701c6f246fc24
koval <koval@DESKTOP-MOAA9A8>
2023-08-15 17:28:55 +0700

    renaming an error in the name

--- 3_selection_sort.py
+++ 3_selection_sort.py
@@ -1,6 +1,6 @@
 import random
 
-def selectionSearch(array):
+def selectionSort(array):
     for i in range(len(array)):
         indexMin = i
         for j in range(i+1,len(array)):
@@ -15,7 +15,7 @@ def selectionSearch(array):
 if __name__ == '__main__':
     arr = [random.randint(-10,100) for i in range(0,11) ] # генерация случайного списка
     print(f"non-sorted array - \n {arr}")
-    print(f"sorted array - \n {selectionSearch(arr)}")
+    print(f"sorted array - \n {selectionSort(arr)}")
     # print(selectionSearch(arr))
     
  
